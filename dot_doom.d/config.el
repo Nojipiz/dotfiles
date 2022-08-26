0aca72dce2d4fc56d883cd6087f61bc1560b4c03
Nojipiz <nojipiz@protonmail.com>
2022-08-26 10:00:39 -0500

    Add DoomEmacs dotfiles (again c:)

---
+++ dot_doom.d/config.el
@@ -0,0 +1,72 @@
+(setq user-full-name "Orlando Vargas"
+      user-mail-address "nojipiz@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "Fira Code" :size 18 :weight 'semi-light))
+(setq doom-theme 'doom-one)
+
+(setq display-line-numbers-type nil)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+(map! :leader
+      :desc "Open Neotree"
+      "e" #'neotree-toggle)
+;; Window Movement
+(map! "C-h" #'evil-window-left)
+(map! "C-l" #'evil-window-right)
+(map! "C-j" #'evil-window-bottom)
+(map! "C-k" #'evil-window-top)
+
+;; Window Manipulation
+(map! :leader
+      :desc "Save File"
+      "w" #'save-buffer)
+(map! :leader
+      :desc "Close Window"
+      "q" #'evil-delete-buffer)
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
