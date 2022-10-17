25f7e179e2c70e8aa415f8fac9956bbbae3e533d
Orlando Vargas <nojipiz@protonmail.com>
2022-10-17 16:41:50 -0500

    Update dot_config/lvim/lua/user/terminal.lua

--- dot_config/lvim/lua/user/terminal.lua
+++ dot_config/lvim/lua/user/terminal.lua
@@ -2,11 +2,12 @@
 -- Terminal Config
 --
 lvim.builtin.terminal.active = true
+lvim.builtin.terminal.direction = "horizontal"
 lvim.builtin.terminal.shell = "/bin/zsh"
+lvim.builtin.terminal.open_mapping = [[<c-t>]]
 
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', 'ii', [[<C-\><C-n>]], opts)
 end
 
