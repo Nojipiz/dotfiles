e4957ec23d71befc82a083c7c48839674a664bc2
Orlando Vargas <nojipiz@protonmail.com>
2022-12-19 17:19:52 -0500

    Add .config/zsh/.zsh_plugins.txt Add .config/zsh/.zsh_plugins.zsh Update .config/zsh/.zshrc

---
+++ dot_config/zsh/dot_zsh_plugins.zsh
@@ -0,0 +1,4 @@
+fpath+=( /home/nojipiz/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/nojipiz/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/nojipiz/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/nojipiz/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
